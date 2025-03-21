8e6a6201f221c424ce402799de91d980b9f07394
minhneee <nguyennhatminh2006ad@gmail.com>
2025-03-21 08:52:56 +0700

    Delete NguyenNhatMinh_1871020395_18-05 directory

--- NguyenNhatMinh_1871020395_18-05/js/purchanse.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('.thumbnail').click(function() {
-        $('.thumbnail').removeClass('active');
-        $(this).addClass('active');
-        $('.img-fluid').attr('src', $(this).data('src'));
-    });
-});
